40d299a86c9943b7f5584333533118f46662e8b9
sudais <sudaisbinsohail@gmail.com>
2024-04-16 23:04:15 +0500

    allergies medical-condition and specific-medication end point

--- healthcare/healthprofile/urls.py
+++ healthcare/healthprofile/urls.py
@@ -2,5 +2,8 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('create-health-profile/',create_health_profile)
+    path('create-health-profile/',create_health_profile),
+    path('get-allergies/',get_allergies),
+    path('get-medical-condition/',get_medical_condition),
+    path('get-specific-medication/',get_specific_medication)
 ]
--- healthcare/healthprofile/views.py
+++ healthcare/healthprofile/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status
 from rest_framework.decorators import api_view , permission_classes
 from rest_framework.response import Response
-from .models import HealthProfile
-from .serializers import HealthProfileSerializer
+from .models import *
+from .serializers import *
 from rest_framework.permissions import IsAuthenticated
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -16,3 +17,32 @@ def create_health_profile(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_allergies(request):
+            allergies = Allergy.objects.all()
+            allergies = AllergySerializer(allergies,many=True)
+            return Response(allergies.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_medical_condition(request):
+            medical_condition = MedicalCondition.objects.all()
+            medical_condition = MedicalConditionSerializer(medical_condition,many=True)
+            return Response(medical_condition.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_specific_medication(request):
+            specific_medication = SpecificMedication.objects.all()
+            specific_medication = SpecificMedicationSerializer(specific_medication,many=True)
+            return Response(specific_medication.data, status=status.HTTP_200_OK)
+       
+
+
+
