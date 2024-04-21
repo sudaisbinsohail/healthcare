f257e2cf72b3d4b567221c97dcb16419cc90af7e
sudais <sudaisbinsohail@gmail.com>
2024-04-21 16:09:49 +0500

    update app id code

--- healthcare/agora_video_call/views.py
+++ healthcare/agora_video_call/views.py
@@ -7,6 +7,8 @@ import uuid
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 import pusher
+import string
+import random
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -22,7 +24,8 @@ def generate_agora_token(request):
         receiver_id=2
         # return Response({'error': 'Please provide sender and receiver id'}, status=status.HTTP_400_BAD_REQUEST)
 
-    channel_name = f'call-{uuid.uuid4()}-{sender_id}-{receiver_id}' 
+    # channel_name = f'call-{uuid.uuid4()}-{sender_id}-{receiver_id}' 
+    channel_name = generate_random_string(12)
 
     AGORA_APP_ID = 'dace37e811464511a92ce19e225e66fa'
     AGORA_APP_CERTIFICATE = '49a559abc1ff4cc0a6cec32d508fb081'
@@ -36,22 +39,28 @@ def generate_agora_token(request):
     }, AGORA_APP_CERTIFICATE, algorithm='HS256')
 
       
-    pusher_client = pusher.Pusher(
-       app_id = "1790808",
-       key = "7eea74474e4ac958eed6",
-       secret = "5b301ddeca15cfb9abaf",
-       cluster = "ap2",
-       ssl=True
-    )
+    # pusher_client = pusher.Pusher(
+    #    app_id = "1790808",
+    #    key = "7eea74474e4ac958eed6",
+    #    secret = "5b301ddeca15cfb9abaf",
+    #    cluster = "ap2",
+    #    ssl=True
+    # )
 
     
-    pusher_client.trigger('video-call', 'start-call', {
-        'sender_id': sender_id,
-        'receiver_id': receiver_id,
-        'channel_name': channel_name,
-        'token': token
-    })
+    # pusher_client.trigger('video-call', 'start-call', {
+    #     'sender_id': sender_id,
+    #     'receiver_id': receiver_id,
+    #     'channel_name': channel_name,
+    #     'token': token
+    # })
 
 
     return Response({'token': token, 'channel_name': channel_name, 'app_id':AGORA_APP_ID}, status=status.HTTP_200_OK)
 
+
+
+#helper to generate random string
+def generate_random_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for _ in range(length))
