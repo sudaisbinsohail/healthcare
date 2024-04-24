31c036d06c001e0cfd8fe95c3d31efc5b710c320
sudais <sudaisbinsohail@gmail.com>
2024-04-24 23:44:45 +0500

    update time slots

--- healthcare/doctors/views.py
+++ healthcare/doctors/views.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
-from .models import DoctorProfile
+from .models import DoctorProfile , AvailabilitySlots
 from healthcare.helper.responses import success_response
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view , permission_classes
 from rest_framework.permissions import IsAuthenticated
-
+from .serializers import *
+from datetime import timedelta , datetime
 
 
 @api_view(['GET'])
@@ -43,6 +44,7 @@ def all_doctors_view(request):
     doctor_data = []
     for doctor in doctors_queryset:
         doctor_data.append({
+            'id':doctor.id,
             'username': doctor.user.user_name,
             'gender':doctor.user.gender,
             'personal_profile': doctor.personal_profile,
@@ -64,9 +66,51 @@ def all_doctors_view(request):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_doctor_slots(request):
-    doctor_id = request.GET.get('doctor_id')
-    doctor_profile_id = DoctorProfile.objects.get(doctor_id = doctor_id)
-    print(doctor_id)
-    print(doctor_profile_id)
+    id = request.GET.get('doctor_id')
+    if id:
+        try:
+            doctor_profile = DoctorProfile.objects.get(id = id)
+        except Exception as e :
+            return Response({"message":"Sorry user Does Not exist"},status=status.HTTP_400_BAD_REQUEST)
+    
+    slots = AvailabilitySlots.objects.filter(doctor_id=doctor_profile.id)
+    serializedSlots = AvailabilitySlotsSerializer(slots , many=True)
+    available_slots_by_day = {
+            'Monday': [],
+            'Tuesday': [],
+            'Wednesday': [],
+            'Thursday': [],
+            'Friday': [],
+            'Saturday': [],
+            'Sunday': []
+        }
+
+    for slot in slots:
+            start_time = slot.start_time
+            end_time = slot.end_time
+            break_start_time = slot.break_start_time
+            break_end_time = slot.break_end_time
+            slot_duration = slot.slot_duration
+            day_of_week = slot.day_of_week
+
+            current_time = datetime.combine(datetime.today(), start_time)
+            end_datetime = datetime.combine(datetime.today(), end_time)
+            break_start_datetime = datetime.combine(datetime.today(), break_start_time)
+            break_end_datetime = datetime.combine(datetime.today(), break_end_time)
+
+            while current_time < end_datetime:
+                if break_start_datetime <= current_time < break_end_datetime:
+                    current_time = break_end_datetime
+                    continue
+
+                end_slot_time = min(current_time + slot_duration, end_datetime)
+                available_slots_by_day[day_of_week].append({
+                    'start_time': current_time.time(),
+                    'end_time': end_slot_time.time()
+                })
+
+                current_time += slot_duration
+
+    return JsonResponse(success_response(message="Doctors Avaliability Timings successfully", data=available_slots_by_day), status=status.HTTP_200_OK)
 
 
